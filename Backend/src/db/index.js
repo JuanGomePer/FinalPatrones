7a77344b875fde969c56fd145babcb8ac6360627
juangomepere <juangomeper@gmail.com>
2025-11-23 21:47:22 -0500

    fixes

--- Backend/src/db/index.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/chatdb';
-const pool = new Pool({ connectionString });
-
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-  pool
-};
